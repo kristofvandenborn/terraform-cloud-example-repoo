caeddaf674bdcec0911e35a7da55b50f546a98c5
Kristof Vandenborn <kristof.vandenborn@davinsi.com>
2025-05-21 19:26:35 +0200

    Changed region

--- versions.tf
+++ versions.tf
@@ -5,12 +5,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
     }
   }
 
